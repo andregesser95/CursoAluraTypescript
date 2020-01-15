89fe0cd49525580aaad6cb61ab607709369e8f31
andregesser95 <andre.alfonsog@gmail.com>
2020-01-15 15:37:27 -0300

    Alterações Decorrentes do Curso
    Interface de função

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,27 +1,41 @@
 import { MensagemView, NegociacoesView } from "../views/index";
-import { Negociacoes, Negociacao } from "../models/index";
+import { Negociacoes, Negociacao, NegociacaoParcial } from "../models/index";
+import { domInject, throttle } from "../helpers/decorators/index";
+import { NegociacaoService } from "../services/index";
 
 export class NegociacaoController {
+
+    @domInject('#data')
     private _inputData: JQuery;
+
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+
+    @domInject('#valor')
     private _inputValor: JQuery;
+
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
 
+    private _service = new NegociacaoService();
+
     constructor() {
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
 
-    adiciona(event: Event) {
+    @throttle()
+    adiciona() {
 
-        event.preventDefault();
-        
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if (!this._ehDiaUtil(data)) {
+            this._mensagemView.update('Somente negociações em dias úteis!!');
+            return
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')),
+            data,
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         );
@@ -31,5 +45,39 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
 
         this._mensagemView.update('Negociação adicionada com sucesso!');
+
     }
+
+    private _ehDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.Domingo && data.getDay() != DiaDaSemana.Sabado;
+    }
+
+    @throttle()
+    importaDados() {
+
+        this._service
+            .obterNegociacoes(res => {
+                if (res.ok) {
+                    return res;
+                } else {
+                    throw new Error(res.statusText);
+                }
+            })
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao =>
+                    this._negociacoes.adiciona(negociacao));
+
+                this._negociacoesView.update(this._negociacoes);
+            });
+    }
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terça,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
---
+++ app/ts/helpers/decorators/index.ts
@@ -0,0 +1,3 @@
+export * from './logarTempoDeExecucao';
+export * from './throttle';
+export * from './domInject';
